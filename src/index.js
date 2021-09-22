0622b04be2ddb79a28c36c8b5e695491540e1110
Kenny Ortega <kennyaoc@gmail.com>
2021-09-22 13:16:18 -0400

    Fix linter and JS files

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import * as item from './items';
 
 const items = [
   {
-    description: '3',
+    description: 'Task 3',
     completed: true,
     number: 3,
   },
   {
-    description: '4',
+    description: 'Task 4',
     completed: false,
     number: 4,
   },
 
   {
-    description: '1',
+    description: 'Task 1',
     completed: true,
     number: 1,
   },
