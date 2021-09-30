a739db83c16dab0f2f3d24bbef0bfee29bfabc3f
SunnySparks <Josediazt6@outlook.com>
2021-09-30 15:30:05 -0500

    Create tests for updating the list

--- src/__test__/addRemoveItem.test.js
+++ src/__test__/addRemoveItem.test.js
@@ -2,19 +2,32 @@
 //import {jest} from '@jest/globals'
 //const addRem = jest.createMockFromModule('../addRemoveItem')
 import { addItem } from '../addRemoveItem';
+import { updateDescription } from '../addRemoveItem';
 
 const list = [];
 const item1 = { description: 'testing1', completed: false, index: 1};
 const item2 = { description: 'testing2', completed: false, index: 2};
+const index1 = list.length;
+const index2 = list.length;
 
-describe('test', () => {
+describe('Adding test', () => {
     test('adding a test element', () => {
         addItem(list, item1);
         expect(list.length).toBe(1);
-    })
-
+    }),
     test('adding a test element', () => {
         addItem(list, item2);
         expect(list.length).toBe(2);
+    })
+});
+
+describe('Updating array test', () => {
+    test('updates the description of the first element in the list', () => {
+        updateDescription(list, index1, 'hello');
+        expect(list[index1].description).toBe('hello');
+    }),
+    test('updates the description of the second element in the list', () => {
+        updateDescription(list, index2, 'goodbye');
+        expect(list[index2].description).toBe('goodbye');
     })
 });
