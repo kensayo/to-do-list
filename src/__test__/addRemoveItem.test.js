4f0fd826a5f36321ade83df790d210203ceaf767
SunnySparks <Josediazt6@outlook.com>
2021-09-30 16:28:10 -0500

    Fix linter errors

--- src/__test__/addRemoveItem.test.js
+++ src/__test__/addRemoveItem.test.js
@@ -1,44 +1,43 @@
-//const { addItem } = require ('../addRemoveItem');
-//import {jest} from '@jest/globals'
-//const addRem = jest.createMockFromModule('../addRemoveItem')
+// const { addItem } = require ('../addRemoveItem');
+// import {jest} from '@jest/globals'
+// const addRem = jest.createMockFromModule('../addRemoveItem')
 import { addItem, updateDescription, removeItem } from '../addRemoveItem';
 
 const list = [];
-const item1 = { description: 'testing1', completed: false, index: 1};
-const item2 = { description: 'testing2', completed: false, index: 2};
+const item1 = { description: 'testing1', completed: false, index: 1 };
+const item2 = { description: 'testing2', completed: false, index: 2 };
 const index1 = list.length;
 const index2 = list.length;
-document.body.innerHTML = '<ul id="list" class="list-container"></ul>'
 
 describe('Adding test', () => {
-    test('adding a test element', () => {
-        addItem(list, item1);
-        expect(list.length).toBe(1);
-    }),
-    test('adding a test element', () => {
-        addItem(list, item2);
-        expect(list.length).toBe(2);
-    })
+  test('adding a test element', () => {
+    addItem(list, item1);
+    expect(list.length).toBe(1);
+  });
+  test('adding a test element', () => {
+    addItem(list, item2);
+    expect(list.length).toBe(2);
+  });
 });
 
 describe('Updating array test', () => {
-    test('updates the description of the first element in the list', () => {
-        updateDescription(list, index1, 'hello');
-        expect(list[index1].description).toBe('hello');
-    }),
-    test('updates the description of the second element in the list', () => {
-        updateDescription(list, index2, 'goodbye');
-        expect(list[index2].description).toBe('goodbye');
-    })
+  test('updates the description of the first element in the list', () => {
+    updateDescription(list, index1, 'hello');
+    expect(list[index1].description).toBe('hello');
+  });
+  test('updates the description of the second element in the list', () => {
+    updateDescription(list, index2, 'goodbye');
+    expect(list[index2].description).toBe('goodbye');
+  });
 });
 
 describe('Deleting array test', () => {
   test('remove second item from the list', () => {
-      removeItem(list.length - 1, list);
-      expect(list.length).toBe(1);
-  }),
+    removeItem(list.length - 1, list);
+    expect(list.length).toBe(1);
+  });
   test('remove first item from the list', () => {
-      removeItem(list.length - 1,list);
-      expect(list.length).toBe(0);
-  })
+    removeItem(list.length - 1, list);
+    expect(list.length).toBe(0);
+  });
 });
