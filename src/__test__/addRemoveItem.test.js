f0bc004e30abec4e85e91db82aeba5c281b5d0fe
SunnySparks <Josediazt6@outlook.com>
2021-09-30 15:09:38 -0500

    Create addItem tests

--- src/__test__/addRemoveItem.test.js
+++ src/__test__/addRemoveItem.test.js
@@ -4,10 +4,17 @@
 import { addItem } from '../addRemoveItem';
 
 const list = [];
+const item1 = { description: 'testing1', completed: false, index: 1};
+const item2 = { description: 'testing2', completed: false, index: 2};
 
 describe('test', () => {
-    test('test test', () => {
-      addItem(list, "my Item");
-      expect(list.length).toBe(1);
-    });
+    test('adding a test element', () => {
+        addItem(list, item1);
+        expect(list.length).toBe(1);
+    })
+
+    test('adding a test element', () => {
+        addItem(list, item2);
+        expect(list.length).toBe(2);
+    })
 });
