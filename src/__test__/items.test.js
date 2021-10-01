0e48b6e2a82ffa8a8c446d9f3295d5e25fdd9062
Kenny Ortega <kennyaoc@gmail.com>
2021-10-01 11:39:31 -0400

    Testing with localStorage

--- src/__test__/items.test.js
+++ src/__test__/items.test.js
@@ -1,16 +1,29 @@
 import { updateStatusItem, textDecorate } from '../items';
-import LocalStorage from '../__mocks__/LocalStorage.js';
-
+import { loadLocalStorage, updateLocalStorage } from '../storage';
+import jest from '@jest/globals'
+jest.mock('../localStorage.js');
 
 global.localStorage = new LocalStorage();
+let list = []
+list = [{    
+        description: 'Test 1',
+        completed: false,
+        number: 1},
+        {
+        description: 'Test 1',
+        completed: false,
+        number: 2}
+    ];
+updateLocalStorage();
 const index = list.index;
 const text = 'Sample text';
-const condition = (list[index].completed) ? list[index].completed = false : list[index].completed = true;
+//const condition = (list[index].completed) ? list[index].completed = false : list[index].completed = true;
+
 
 describe('Update status of the index', () => {
 
     test('it will toggle the status of the index to complete when clicked', () => {
-        condition(true)(expect(updateStatusItem(index, text).toBe(false)));
+        expect(loadLocalStorage().length).toBe();
     });
 
   });
